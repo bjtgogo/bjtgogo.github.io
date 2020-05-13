33001946e941923f7ffcc2a39dfea8ac978190dd
bjtgogo <62091042+bjtgogo@users.noreply.github.com>
2020-05-13 12:17:26 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>test MyWeb</title>
+		<meta charset="utf-8" />
+
+        
+	</head>
+	<body>
+    
+  
+
+<h1>TEST MY WEB</h1>
+
+<h2>HELLO WORLD!</h2>
+<p>2020-05-13</p>
+
+
+   
+
+</body>
+</html>
